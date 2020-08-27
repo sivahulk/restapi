966491b73dbc0d02318ed61098d08f0e80b1a348
ml-pyspark <sivajothianglechrist@gmail.com>
2020-08-27 19:10:49 +0530

    fixed not importing db

--- app.py
+++ app.py
@@ -5,24 +5,17 @@ from security import authenticate, identity
 from resources.user import UserRegistor
 from resources.item import Item,ItemsList
 from resources.store import Store,StoreList
-from db import db
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONs']=False #this will turn off track in flask and not the flask-sqlalchemy. why we used False because we imported flask-sqlalchemy so it will have its own tracking.
-app.secret_key='hulk' 
+app.secret_key='hulk'
 api=Api(app)
-
-@app.before_first_request #this decorator function will execute before first request so it will create table which we used in py files
-def create_all_table():
-    db.create_all()
 
 jwt=JWT(app,authenticate, identity) #/auth
 
 items=[]
 
-
-    
 #adding resource to api
 api.add_resource(Store,'/store/<string:name>')
 api.add_resource(Item,'/item/<string:name>')#http://127.0.0.1:5000/item/chair
@@ -31,6 +24,6 @@ api.add_resource(ItemsList,'/items')
 api.add_resource(UserRegistor,'/registor')
 
 if __name__ == '__main__':
-    #from db import db
+    from db import db
     db.init_app(app)
     app.run(port=5000)
