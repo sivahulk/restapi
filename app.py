ce29082f1277ee65ae7103462443710c4bafabbe
ml-pyspark <sivajothianglechrist@gmail.com>
2020-08-27 19:52:18 +0530

    added postgreSQL env var and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -7,7 +9,7 @@ from resources.item import Item,ItemsList
 from resources.store import Store,StoreList
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL','sqlite:///data.db')# if u r running this in local os will take sqlite because second parameter is default one. if it is running in heroku it willtake 1st param
 app.config['SQLALCHEMY_TRACK_MODIFICATIONs']=False #this will turn off track in flask and not the flask-sqlalchemy. why we used False because we imported flask-sqlalchemy so it will have its own tracking.
 app.secret_key='hulk'
 api=Api(app)
